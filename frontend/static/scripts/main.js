ce646f0bd5479b3402574fb63fc9eb6488aec206
MikhailTo <toshkin.mikhail@yandex.ru>
2024-08-04 09:43:33 +0300

    change template: add includes

--- frontend/static/scripts/main.js
+++ frontend/static/scripts/main.js
@@ -1,18 +1,24 @@
-console.log("I'm working!")
-
 document.querySelector('.menu-icon').addEventListener('click', function() {
     document.querySelector('.nav-menu').classList.toggle('active');
 });
 
-
-
 document.querySelectorAll('.list-item').forEach(item => {
-    item.addEventListener('click', (event) => {
-        if (!event.target.closest('.list-icons') && !event.target.closest('.toggle-button')) {
-            const content = item.querySelector('.list-content');
-            const toggleButton = item.querySelector('.toggle-button');
-            content.classList.toggle('active');
-            toggleButton.classList.toggle('active');
-        }
+    const clickableArea = item.querySelector('.list-item-clickable');
+    const content = item.querySelector('.list-content');
+    const toggleButton = item.querySelector('.toggle-button');
+
+    function toggleContent() {
+        content.classList.toggle('active');
+        toggleButton.classList.toggle('active');
+    }
+
+    clickableArea.addEventListener('click', (event) => {
+        event.preventDefault();
+        toggleContent();
+    });
+
+    toggleButton.addEventListener('click', (event) => {
+        event.stopPropagation();
+        toggleContent();
     });
 });
