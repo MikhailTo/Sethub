a0150918be548669fb9da4a9b673669cd5176bdc
Mike <34812500+MikhailTo@users.noreply.github.com>
2024-08-07 04:12:32 +0000

    User already registered

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -8,5 +8,5 @@ router = APIRouter()
 async def create_user(user: users.UserCreate):
     db_user = await users_utils.get_user_by_email(email=user.email)
     if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(status_code=400, detail="User already registered")
     return await users_utils.create_user(user=user)
