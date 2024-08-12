9f4ef38c476a2dc7dae2e1636394d866ada23b25
MikhailTo <toshkin.mikhail@yandex.ru>
2024-08-12 17:53:22 +0300

    "Added docstring to config.py, reformatted code, and updated type hints and environment variable defaults."

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,3 +1,16 @@
+'''
+Configuration Module
+
+This module contains configuration settings and parameters for the backend application.
+It may include environment variables, database settings, API keys, and other configuration options.
+
+Usage:
+    Import this module to access various configuration settings throughout the application.
+
+Note:
+    Ensure that sensitive information is properly secured and not exposed in the codebase.
+'''
+
 from os import getenv as get
 from pathlib import Path
 
@@ -5,16 +18,17 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    """Base settings class for the application."""
+
     # Database
     DB_DIALECT:     str     =   get("DB_DIALECT", "postgresql")
     DB_DRIVERNAME:  str     =   get("DB_DRIVERNAME", "asyncpg")
-    DB_USERNAME:    str     =   get("DB_USERNAME", "postgres") 
+    DB_USERNAME:    str     =   get("DB_USERNAME", "postgres")
     DB_PASSWORD:    str     =   get("DB_PASSWORD", "postgres")
     DB_HOST:        str     =   get("DB_HOST", "localhost")
-    DB_PORT:        int     =   get("DB_PORT", 5432)
+    DB_PORT:        int     =   get("DB_PORT", "5432")
     DB_NAME:        str     =   get("DB_NAME", "db_sethub")
-    DB_ECHO:        bool    =   get("DB_ECHO", True)
-
+    DB_ECHO:        bool    =   get("DB_ECHO", "True")
 
     # Folder names
     BACKEND_FOLDER_NAME:    Path = Path('backend')
@@ -23,7 +37,7 @@ class Settings(BaseSettings):
     MEDIA_FOLDER_NAME:      Path = Path('media')
     STATIC_FOLDER_NAME:     Path = Path('static')
     TEMPLATES_FOLDER_NAME:  Path = Path('templates')
-    
+ 
     # Paths
     MAIN_PATH:          Path = Path(__file__).resolve().parents[3]
     BACKEND_PATH:       Path = MAIN_PATH / BACKEND_FOLDER_NAME
