0cecd2d6c45dd4cbfd4785f5aeaf4cec76420d42
Mike <34812500+MikhailTo@users.noreply.github.com>
2024-08-14 06:07:27 +0000

    add const and version for main (app and uvicorn params)

---
+++ backend/app/const.py
@@ -0,0 +1,23 @@
+from typing import Final, Dict, Any
+from backend.app.version import __version__
+
+# Application params
+APP_TITLE:          Final[str]   =   "Sethub"
+APP_DESCRIPTION:    Final[str]   =   "Sethub is an information portal designed for creating, managing, and sharing various types of lists."
+
+app_params:   Final[Dict[str, Any]] = {
+    "title": APP_TITLE, 
+    "description": APP_DESCRIPTION,
+    "version": __version__,
+    "swagger_ui_parameters": {"defaultModelsExpandDepth": -1},
+    }
+
+# Uvicorn params
+HOST:           Final   =   "0.0.0.0"
+PORT:           Final   =   8000
+
+uvicorn_params:   Final[Dict[str, Any]] = {
+    "host": HOST, 
+    "port": PORT
+    }
+
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 
-from backend.app.routers import main, posts, users
+from backend.app.const import app_params, uvicorn_params
+from backend.app.routers import main, users, posts
 
-app = FastAPI()
+
+app = FastAPI(**app_params)
 
 app.include_router(main.router)
 app.include_router(posts.router)
 app.include_router(users.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, **uvicorn_params)
